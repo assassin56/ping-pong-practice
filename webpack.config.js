028439bf5ec0fbb349a07df17d2245217da3123d
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-02-03 17:17:37 -0800

    add webpack development server plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
 
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  devTool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
   },
   plugins: [
     new HtmlWebpackPlugin({
