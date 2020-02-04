0394b40dca4a58b32b2428ef3ba08556d8c42e76
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-02-03 16:45:28 -0800

    update repo

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,23 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title : 'Ping Pong',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rule: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 };
